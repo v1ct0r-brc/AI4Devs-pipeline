74f8e7725dfbcb791153cd9f07b48a36778c629b
Victor Allar <vallar@invap.com.ar>
2026-07-30 00:06:07 -0300

    vuelvo puerto a 5432

--- backend/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-datasource db {
-  provider = "postgresql"
-  url      = "postgresql://LTIdbUser:D1ymf8wyQEGthFR1E9xhCq@localhost:5433/LTIdb"
-}
-
-generator client {
-  provider      = "prisma-client-js"
-  binaryTargets = ["native", "debian-openssl-3.0.x"]
-}
